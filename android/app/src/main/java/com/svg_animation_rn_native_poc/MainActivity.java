0070116a80e0cdd519a75f972e6ca888713ec00d
Pete <peteburquette@gmail.com>
2022-06-23 19:45:09 +0100

    first commit

---
+++ android/app/src/main/java/com/svg_animation_rn_native_poc/MainActivity.java
@@ -0,0 +1,15 @@
+package com.svg_animation_rn_native_poc;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Svg_Animation_RN_Native_Poc";
+  }
+}
